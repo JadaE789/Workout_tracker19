cfe8d07218e21489b536aa640eb4237b0570bc8d
Jada Desormeaux <JadaE789@gmail.com>
2021-06-22 21:07:46 -0400

    routes and server.js

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,15 +1,23 @@
+// Import Express router
+const router = require("express").Router();
+
+//Import path
 const path = require("path");
 
-module.exports = function(app) {
+// GET Request : Fitness Tracker Home page
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-    
-  
-    app.get("/exercise", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
-  
-    app.get("/stats", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
-  
-};
+// GET Request : Exercise Page
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// GET Request : Stats Page
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+// Export HTML routes
+module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
-require("./seeders/seed"); 
+const PORT = process.env.PORT || 3001;
 
-const PORT = process.env.PORT || 3002;
-
+// Create Express app
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
+// Create mongoose database connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_trackerdb", {
-    useNewUrlParser: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
-//require(apiRoute)(app);
-require("./routes/htmlroutes")(app);
-require("./routes/apiroutes")(app);
+// routes
+app.use(require("./routes/apiroutes.js"));
+app.use(require("./routes/htmlroutes.js"));
 
-app.listen(PORT,() => {
-    console.log(`App running on ${PORT}!`);
+// Listen to request 
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
